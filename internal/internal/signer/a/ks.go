04e14e7d280c5913d5ac816b8bb8fff717555d0a
storezhang <storezhang@gmail.com>
2026-01-08 19:21:19 +0800

    feat(厂商): 支持金山云

--- internal/internal/signer/a/ks.go
+++ internal/internal/signer/a/ks.go
@@ -14,22 +14,21 @@ import (
 var _ internal.Signer = (*Ks)(nil)
 
 type Ks struct {
-	pattern   string
-	key       string
-	signature string
+	pattern string
+	key     string
 }
 
 func NewKs(key string) *Ks {
 	return &Ks{
-		pattern:   "%s%s%d",
-		key:       key,
-		signature: "sign",
+		pattern: "%s%s%d",
+		key:     key,
 	}
 }
 
 func (k *Ks) Sign(url *url.URL, _ time.Duration) (err error) {
 	now := time.Now().Unix()
-	key := cryptor.New(fmt.Sprintf(k.pattern, url.EscapedPath(), now)).Md5().Hex()
+	hash := fmt.Sprintf(k.pattern, k.key, url.EscapedPath(), now)
+	key := cryptor.New(hash).Md5().Hex()
 	sb := gox.StringBuilder(url.RawQuery)
 	if "" == url.RawQuery {
 		sb.Append(constant.Question)
